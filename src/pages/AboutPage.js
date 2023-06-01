bbeac169b604d525c7005c558a4b33fdcdafdd10
Shomari Pope <shomaripope@gmail.com>
2023-05-31 23:24:56 -0400

    added more responsiveness to navbar with media queries and added shopping cart icon

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import ReviewSection from '../components/ReviewSection';
-import FeatuedCandy from '../components/FeatuedCandy';
-import FeaturedCrafts from '../components/FeaturedCrafts';
 import Footer from '../components/Footer';
 
 export default function AboutPage() {
