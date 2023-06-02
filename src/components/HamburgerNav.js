cf234338b1a7eef6f282b87cb4fcc4a6e5d0f98f
Shomari Pope <shomaripope@gmail.com>
2023-06-01 22:15:49 -0400

    added responsive hamburger nav menu

---
+++ src/components/HamburgerNav.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import 'material-icons';
+import ShoppingCartIcon from './ShoppingCartIcon';
+
+export default function HamburgerNav() {
+  return (
+    <div><section class="p-menu1">
+    <nav id="navbar" class="navigation" role="navigation">
+      <input id="toggle1" type="checkbox" />
+      <label class="hamburger1" for="toggle1">
+        <div class="top"></div>
+        <div class="meat"></div>
+        <div class="bottom"></div>
+      </label>
+    
+      <nav class="menu1">
+        <ul>
+          <li><Link to="/candy" className="link1">Candy</Link></li>
+          <li><Link to="/crafts" className="link1">Crafts</Link></li>
+          <li><Link to="/about" className="link1">About</Link></li>
+          <li><Link to="/contact" className="link1">Contact</Link></li>
+          <ShoppingCartIcon />
+        </ul>
+      </nav>
+  </nav>
+  </section></div>
+  )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import raysLogo from '../images/rayslogo250px.png';
 import { Link } from 'react-router-dom';
 import ShoppingCartIcon from './ShoppingCartIcon';
+import HamburgerNav from './HamburgerNav';
 
 
 export default function Header() {
@@ -13,11 +14,12 @@ export default function Header() {
           </Link>
         </div>
         <ul>
-          <li><Link to="/candy">Candy</Link></li>
+          {/* <li><Link to="/candy">Candy</Link></li>
           <li><Link to="/crafts">Crafts</Link></li>
           <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <ShoppingCartIcon />
+          <li><Link to="/contact">Contact</Link></li> */}
+          {/* <ShoppingCartIcon /> */}
+          <HamburgerNav />
         </ul>
     </div>
   )
