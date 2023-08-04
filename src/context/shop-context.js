e5976165d9bc020af3585ddc4a75a984d9f1deda
Shomari Pope <shomaripope@gmail.com>
2023-08-03 21:59:58 -0400

    added subtotal and quantity selection

--- src/context/shop-context.js
+++ src/context/shop-context.js
@@ -1,3 +1,27 @@
+// import React, { createContext, useState } from 'react';
+
+// export const shopContext = createContext(null);
+
+// export const ShopContextProvider = (props) => {
+//   const [cartItems, setCartItems] = useState([]);
+
+//   const addToCart = (product) => {
+//     setCartItems((prev) => [...prev, product]);
+//   };
+
+//   const removeFromCart = (productId) => {
+//     setCartItems((prev) => prev.filter(item => item.id !== productId));
+//   };
+
+//   const contextValue = { cartItems, addToCart, removeFromCart };
+
+//   return (
+//     <shopContext.Provider value={contextValue}>
+//       {props.children}
+//     </shopContext.Provider>
+//   );
+// };
+
 import React, { createContext, useState } from 'react';
 
 export const shopContext = createContext(null);
@@ -6,11 +30,32 @@ export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (product) => {
-    setCartItems((prev) => [...prev, product]);
+    setCartItems((prev) => {
+      const itemInCart = prev.find((item) => item.id === product.id);
+      if (itemInCart) {
+        // If the item is already in the cart, increase its quantity by 1
+        const updatedCart = prev.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+        return updatedCart;
+      } else {
+        // If the item is not in the cart, add it with a quantity of 1
+        return [...prev, { ...product, quantity: 1 }];
+      }
+    });
   };
 
   const removeFromCart = (productId) => {
-    setCartItems((prev) => prev.filter(item => item.id !== productId));
+    setCartItems((prev) => {
+      const updatedCart = prev.map((item) =>
+        item.id === productId
+          ? item.quantity > 1 
+            ? { ...item, quantity: item.quantity - 1 } 
+            : null 
+          : item
+      );
+      return updatedCart.filter((item) => item !== null);
+    });
   };
 
   const contextValue = { cartItems, addToCart, removeFromCart };
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,32 +1,78 @@
+// import React, { useContext } from 'react';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+// import { shopContext } from "../context/shop-context";
+
+// export default function CartPage() {
+//   const { cartItems, removeFromCart } = useContext(shopContext);
+  
+
+//   return (
+//     <div>
+//         <Header />
+//             <div className='cartPage-list'>
+//                 <h1 className='cartPageTitle'>Cart</h1>
+//                     <ul className='cart-list'>
+//                         {cartItems.map(item => (
+//                         <li key={item.id}>
+//                             <div className='product-card'>
+//                                 <img className="product-img" src={item.image} alt={item.title} />
+//                                 <h3>{item.title}</h3>
+//                                 <p className='product-price'>Price: ${item.price}</p>
+//                                 <button className='btn' onClick={() => removeFromCart(item.id)}>Remove From Cart</button>
+//                             </div>
+//                         </li>
+//                         ))}
+//                     </ul>
+//             </div>
+//         <Footer />
+//     </div>
+//   );
+// }
+
+
 import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { shopContext } from "../context/shop-context";
 
+
 export default function CartPage() {
-  const { cartItems, removeFromCart } = useContext(shopContext);
-  
+  const { cartItems, addToCart, removeFromCart } = useContext(shopContext);
+
+  const calculateSubtotal = (item) => {
+    const subtotal = item.price * item.quantity;
+    return subtotal.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  };
 
   return (
     <div>
-        <Header />
-            <div className='cartPage-list'>
-                <h1 className='cartPageTitle'>Cart</h1>
-                    <ul className='cart-list'>
-                        {cartItems.map(item => (
-                        <li key={item.id}>
-                            <div className='product-card'>
-                                <img className="product-img" src={item.image} alt={item.title} />
-                                <h3>{item.title}</h3>
-                                <p className='product-price'>Price: ${item.price}</p>
-                                <button className='btn' onClick={() => removeFromCart(item.id)}>Remove From Cart</button>
-                            </div>
-                        </li>
-                        ))}
-                    </ul>
-            </div>
-        <Footer />
+      <Header />
+      <h1 className='cartPageTitle'>Cart</h1>
+      <div className='cartPage-list'>
+        <ul className='cartItemList'>
+          {cartItems.map(item => (
+            <li key={item.id}>
+              <div className='product-card'>
+                <img className="product-img" src={item.image} alt={item.title} />
+                <h3>{item.title}</h3>
+                <p className='product-price'>Price: ${item.price}</p>
+                <div className='quantity'>
+                    <h6>Quantity</h6>
+                  <button className='btn' onClick={() => removeFromCart(item.id)}>Remove</button>
+                  <span className='item-quantity'>{item.quantity}</span>
+                  <button className='btn' onClick={() => addToCart(item)}>Add</button>
+                </div>
+                <div>
+                <h6>Subtotal</h6>
+                <p className='product-subtotal'>Subtotal: {calculateSubtotal(item)}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Footer />
     </div>
   );
 }
-
